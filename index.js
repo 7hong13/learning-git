977a66a3befeef573152031d3ffd083dde089d1a
7hong13 <ledi000@naver.com>
2019-08-26 05:27:49 +0000

    README.md를 서빙하도록 수정

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const http = require('http');
 const fs = require('fs');
-const port = 3000;
-http.createServer((request, response) => {
-console.log('Request received.');
 
-fs.readFile('view/README.md', (error, data) => {
-console.log('Read file - README.md');
->>>>>>> Stashed changes
-response.writeHead(200, {'Content-Type': 'text/html'});
-response.write(data);
-response.end();
+const port = 3000;
+
+http.createServer((request, response) => {
+   console.log('Request received.');
+
+    fs.readFile('view/README.md', (error, data) => {
+        console.log('Read file - README.md');
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+    });
+
 }).listen(port);
+
 console.log(`Server has stared - port:${port}`);
-});
-}).listen(port);
->>>>>>> Stashed changes
