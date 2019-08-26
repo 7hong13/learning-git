73920d6c280408522b23dd710eb4bacff1c795b1
7hong13 <ledi000@naver.com>
2019-08-26 05:08:26 +0000

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 const port = 3000;
 http.createServer((request, response) => {
 console.log('Request received.');
+
+fs.readFile('view/README.md', (error, data) => {
+console.log('Read file - README.md');
+>>>>>>> Stashed changes
 response.writeHead(200, {'Content-Type': 'text/html'});
-response.write('Hello Git!');
+response.write(data);
 response.end();
 }).listen(port);
 console.log(`Server has stared - port:${port}`);
+});
+}).listen(port);
+>>>>>>> Stashed changes
